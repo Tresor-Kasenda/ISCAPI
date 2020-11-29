26016b503e10bf401f98f6d362f1bbc72b445971
scotttresor <scotttresor@gmail.com>
2020-11-29 15:38:46 +0100

    feat(result):result database blade

---
+++ resources/views/emails/result.blade.php
@@ -0,0 +1,8 @@
+@component('mail::message')
+
+    # Bienvenue {{ $result->username }}-{{ $result->prenom }}
+    Les resultats  ont ete  publier, veillez  consulter  votre telephone  et  votre  mail pour consulter
+
+
+    Merci d'avoir  choisi l'ISC/Lubumbashi
+@endcomponent
